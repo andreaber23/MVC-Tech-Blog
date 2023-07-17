cc6acfdfb27712392efaaf16dab49dd13194c1cb
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-07-17 10:00:38 -0400

    commentRoute updated. still needs to be done the delete comment route and export it

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { BlogPost, Comment, User } = require("../../models");
+const { param } = require("./blogPostRoute");
 
 //get all comments
 router.get("/", async (req, res) => {
@@ -25,15 +26,37 @@ router.get("/", async (req, res) => {
 // create comment
 router.post("/", async (req, res) => {
     try {
-      const comment = await Comment.create({
+      const newComment = await Comment.create({
         comment_post: req.body.comment_post,
         post_id: req.body.post_id,
         user_id: req.session.user_id,
       });
   
-      res.status(200).json(comment);
+      res.status(200).json(newComment);
     } catch (err) {
       console.error(err);
       res.status(500).json(err);
     }
   });
+
+  //update
+
+  route.put("/:id", async (req,res) => {
+    try {
+        const updateComment = await Comment.update(req.body, {
+            where:{
+                id: req.params.id
+            },
+        })
+        
+        if (!updateComment[0]){
+            res.status(400).json({ message: "No comment found with that id!" });
+      return;
+        }
+        console.log("comment was updated"),
+        res.status(200).json(updateComment);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+      }
+  })
