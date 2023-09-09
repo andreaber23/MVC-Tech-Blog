427f52ea1b246ec7d893354a4307c264b1454a19
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-09 16:10:29 -0400

    added dashboard route to home routes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,17 +8,13 @@ router.get("/", async (req,res) => {
             include: [
                 {
                  model: User,
-                 attributes: ['name'],
-                },
-                {
-                 model: Comment,
-                 attributes: ['comment_post']
+                 attributes: ['username'],
                 },
             ],
         });
 
         const blogPost = blogPostData.map((blogPost)=>
-         blogPost.get({plain:true}))
+         blogPost.get({plain: true}))
 
         res.render('homepage', { 
             blogPost,
@@ -34,21 +30,22 @@ router.get("/blogPost/:id", withAuth, async (req,res) => {
         const blogPostData = await BlogPost.findByPk(req.params.id, {
             include: [
                 {
-                 model: User,
-                 attributes: ['name'],
-                },
-                {
                  model: Comment,
-                 attributes: [User]
+                 attributes: ['id','title','description', 'date_created','user_id'],
+                 include: {
+                    model: User,
+                    attributes: ['username'],
+                   },
                 },
             ],
         });
 
-        const blogPost = blogPostData.get({plain:true});
+       const comments = blogPostData.comments.map(comment => comment.get({ plain: true }))
 
         res.render('blogPost', { 
-            ...blogPost,
-            logged_in: req.session.logged_in 
+            ...blogPostData.get({ plain: true }),
+            logged_in: req.session.logged_in,
+            comments: comments,
         });
     } catch (err) {
         res.status(500).json(err);
@@ -58,15 +55,38 @@ router.get("/blogPost/:id", withAuth, async (req,res) => {
 router.get('/login', (req, res) => {
    
     if (req.session.logged_in) {
-      res.redirect('/');
+      res.redirect('/dashboard');
       return;
     }
     res.render('login');
   });
 
   router.get("/signup",(req,res)=>{
-    res.render("signup")
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+    return;
+}
+    res.render('signup');
 })
-  
+
+router.get('/dashboard', withAuth, async (req,res) => {
+    try{
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {
+                exclude: ['password']},
+                include:[
+                    {model: BlogPost}],
+            });
+ 
+            const user = userData.get({plain: true });
+            res.render('dashboard', {
+                ...user,
+                logged_in: true,
+            });
+        } catch (err) {
+            res.status(500).json(err);
+          }
+});
+   
   module.exports = router;
   
