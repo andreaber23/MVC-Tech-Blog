21809f199b42bd91f36de75472af985eec237ad2
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-10 10:42:40 -0400

    changed blogPost to Post on seeds folder. now deploying to heroku

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-const postRoutes = require('./postRoutes');
 const userRoutes = require('./userRoutes');
-
+const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
